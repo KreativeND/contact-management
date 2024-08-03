8678008ed7503236a0312d23fc2e99bbb4db41b3
nd <nirajdasari@yahoo.com>
2024-08-03 13:04:10 +0530

    fixing build issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['core-js-pure']
+  },
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
